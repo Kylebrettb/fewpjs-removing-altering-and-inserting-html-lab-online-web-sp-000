6f3be3d37d308e1d464538aad53279107553e64a
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-10-10 16:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = null;
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerText = "Angela is the champion!";
+document.body.appendChild(newHeader);
